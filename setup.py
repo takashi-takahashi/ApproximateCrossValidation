4d495de5f835f0fd6bca9ec3fbbddf8df03c00eb
Takashi <takahashi.t.cc@m.titech.ac.jp>
2017-11-16 18:00:33 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='mfcv_test',
+    version='0.0.1',
+    packages=['mean_field_cv'],
+    author='Takashi TAKAHASHI',
+    author_email='takahashi.t.cc@m.titech.ac.jp',
+    description='Sample Approximate Cross Validation Module',
+)
